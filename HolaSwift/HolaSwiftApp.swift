57d2e301ad8fc49096d401efee281d3331704676
Jorge Gimeno <jgimenolatas@gmail.com>
2025-09-30 15:36:53 +0200

    Initial Commit

---
+++ HolaSwift/HolaSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolaSwiftApp.swift
+//  HolaSwift
+//
+//  Created by Jorge Gimeno Latas on 30/9/25.
+//
+
+import SwiftUI
+
+@main
+struct HolaSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HolaSwiftTests/HolaSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  HolaSwiftTests.swift
+//  HolaSwiftTests
+//
+//  Created by Jorge Gimeno Latas on 30/9/25.
+//
+
+import Testing
+@testable import HolaSwift
+
+struct HolaSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
